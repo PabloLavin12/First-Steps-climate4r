aa77a99ac81bf7030a1f6a4f7f20707e10c82890
Pablo <45602480+PabloLavin12@users.noreply.github.com>
2026-02-16 13:08:09 +0100

    Add files via upload

---
+++ setup_libraries.R
@@ -0,0 +1,22 @@
+# Cargamos paquetes
+
+library(sp)
+library(repr)
+library(dplyr)
+library(pROC)
+
+library(abind)
+library(loadeR)
+library(transformeR)
+library(convertR)
+library(visualizeR)
+library(downscaleR)
+library(climate4R.UDG)
+library(climate4R.climdex)
+library(climate4R.indices)
+library(easyVerification)
+
+library(lattice)
+library(magrittr)
+library(gridExtra)
+library(RColorBrewer)
